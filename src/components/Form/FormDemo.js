99cc78ae378ee9aaa86025a69a1f4a38229961a3
ThuatPT <thuatptpk02720@fpt.edu.vn>
2024-09-02 22:13:45 +0700

    feat: Form react

---
+++ src/components/Form/FormDemo.js
@@ -0,0 +1,11 @@
+function FormDemo() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="" name="useName"></label>
+        <input type="text" name="useName" id="" />
+      </form>
+    </div>
+  );
+}
+export default FormDemo;
